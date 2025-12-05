de2d26a154fc0020a545bbdcff7fc19d1f4f92f7
LeoGynn <92831967+LeoGynn@users.noreply.github.com>
2025-12-05 13:01:22 -0300

    Fix lista.c
    Correção do arquivo lista.c que estava sem a função lista_remover_por_nome

--- Projeto_RPG/lista.c
+++ Projeto_RPG/lista.c
@@ -20,3 +20,13 @@ if (l->tamanho >= CAPACIDADE) return 0;
 l->dados[l->tamanho++] = p;
 return 1;
 }
+
+int lista_remover_por_nome(ListaPersonagens *l, const char *nome) {
+    int idx = lista_encontrar_indice(l, nome);
+    if (idx < 0) return 0;
+    for (int i = idx; i < l->tamanho - 1; i++) {
+        l->dados[i] = l->dados[i+1];
+    }
+    l->tamanho--;
+    return 1;
+}
